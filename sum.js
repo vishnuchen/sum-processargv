557435b825b18f532d8523d08cc46673e815119a
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-01 23:46:26 +0000

    fixed argv

--- sum.js
+++ sum.js
@@ -1,5 +1,5 @@
 //to extract inputs at console [2] 3rd input and so on
-let numOne = Number(process.argvargs[2]); // Number converts string as process.argv outputs string
-let numTwo = Number(process.argvargs[3]);
+let numOne = Number(process.argv[2]); // Number converts string as process.argv outputs string
+let numTwo = Number(process.argv[3]);
 let sumResult = numOne + numTwo;
 console.log(sumResult);
