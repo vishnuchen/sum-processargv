514bc8b88460b4dfa5151470595570166b0cc07c
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-01 23:44:40 +0000

    first commit

---
+++ sum.js
@@ -0,0 +1,5 @@
+//to extract inputs at console [2] 3rd input and so on
+let numOne = Number(process.argvargs[2]); // Number converts string as process.argv outputs string
+let numTwo = Number(process.argvargs[3]);
+let sumResult = numOne + numTwo;
+console.log(sumResult);
